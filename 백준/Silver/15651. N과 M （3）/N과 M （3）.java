10a9f66722244e8b9062b6feff76d43556565555
djsumin <136299367+djsumin@users.noreply.github.com>
2023-09-23 21:37:46 +0900

    [Silver III] Title: N과 M (3), Time: 644 ms, Memory: 308244 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] arr; // 배열
+	static int[] result;
+	static int[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		// n개중에 M개를 뽑자 먼저.
+		arr = new int[N];
+		result = new int[M];
+		visited = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		} // 선택하고자 하는 대상 집합
+		combination(0,0);
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+	private static void combination(int idx, int cnt) {
+		if(cnt == M) {
+			for(int i=0;i<result.length;i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0;i<N;i++) {
+			result[idx]=arr[i];
+			combination(idx+1, cnt+1);
+		}
+	}
+
+	
+
+}
