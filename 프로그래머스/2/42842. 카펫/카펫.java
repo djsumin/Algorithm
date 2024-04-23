c934e10dd1d4f98fa1966edb4aabf18895676049
djsumin <136299367+djsumin@users.noreply.github.com>
2024-04-23 20:19:47 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int sum = brown + yellow;
+        
+        for(int i=3;i<=sum;i++){
+            int col = i; //세로
+            int row = sum / i; // 가로
+            
+            if(row < 3){
+                continue;
+            }
+            
+            if(row >= col){
+                if((row-2)*(col-2)==yellow){
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
