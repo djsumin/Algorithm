b5ab0b500e2d6153e4fa878ad0b83a6a76ba633b
djsumin <136299367+djsumin@users.noreply.github.com>
2024-10-24 08:12:52 +0900

    [Silver II] Title: DFS와 BFS, Time: 232 ms, Memory: 23620 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,65 +1,68 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static boolean[] visited;
-	static int [][] adjArr;
-	static Queue<Integer> queue = new LinkedList<>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());//정점
-		int M = Integer.parseInt(st.nextToken());//간선
-		int V = Integer.parseInt(st.nextToken());//탐색시작점
-		adjArr = new int[N+1][N+1];
-		visited = new boolean[N+1];
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			adjArr[a][b]=1;
-			adjArr[b][a]=1;
-		}//입력받기
-//		System.out.println(Arrays.deepToString(adjArr));
-		DFS(V);
-		visited = new boolean[N+1]; //방문배열 초기화
-		System.out.println();
-		BFS(V);
-	}
-	
-	 static void BFS(int i) {
-		 queue.add(i);
-		 visited[i] = true;
-		 while(!queue.isEmpty()) {
-			 int t = queue.poll();
-			 System.out.print(t +" ");
-			 for(int j =1 ;j<adjArr.length;j++) {
-				 if(!visited[j] && adjArr[t][j] ==1) {
-					 queue.add(j);
-					 visited[j]=true;
-				 }
-			 }
-		 }
-	}
-	 
-	static void DFS(int i) {
-		visited[i]=true;
-		System.out.print(i+" ");
-		
-		for(int a=1;a<N+1;a++) {
-			if(!visited[a] && adjArr[i][a]==1) {
-				DFS(a);
-			}
-		}
-	}
-	
+    static StringBuilder sb;
+    static int n, m, v;
+    static int[][] adjarr;
+    static boolean[] visit;
+    static Queue<Integer> q = new LinkedList<>();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken()); // 시작점
+
+        adjarr = new int[n+1][n+1];
+        visit = new boolean[n+1];
+
+        for(int i= 0; i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            adjarr[start][end] = 1;
+            adjarr[end][start] = 1;
+        }
+
+        dfs(v);
+        sb.append("\n");
+        visit = new boolean[n+1];
+
+        bfs(v);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void bfs(int start) {
+        q.add(start);
+        visit[start] = true;
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+            sb.append(now+" ");
+
+            for(int i=1;i<=n;i++){
+                if(!visit[i] && adjarr[now][i] == 1){
+                    visit[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+
+    }
+
+    private static void dfs(int start) {
+        visit[start] = true;
+        sb.append(start + " ");
+
+        for(int i=0;i<=n;i++){
+            if(!visit[i] && adjarr[start][i] == 1){
+                dfs(i);
+            }
+        }
+    }
 }
