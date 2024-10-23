4954f8abfbe065855efa446623ddf0b727bdc39f
djsumin <136299367+djsumin@users.noreply.github.com>
2024-10-23 20:47:35 +0900

    [level 2] Title: 구명보트, Time: 11.61 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,22 +3,33 @@ import java.io.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
         
         Arrays.sort(people);
+        
+        int boat = 0;
         int min = 0;
-        for(int i=people.length-1;i>=min;i--){
-            if(people[i] + people[min] <= limit){
-                min++;//최소 조건 합격하면 태워
-                answer++;
+        int size = people.length;
+        int max = size-1;
+        
+        for(int i = 0;i<size;i++){
+            if(people[min] + people[max] <= limit){
+                min++;
+                max--;
+                boat++;
             }else{
-                //최소랑 합쳤는데도 안되면 혼자타
-                answer++;
+                max--;
+                boat++;
             }
             
+            if(min > max){
+                break;
+            }
+            if(min==max){
+                boat++;
+                break;
+            }
         }
         
-        
-        return answer;
+        return boat;
     }
 }
