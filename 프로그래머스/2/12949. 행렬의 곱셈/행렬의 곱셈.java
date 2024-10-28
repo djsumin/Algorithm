2250d4200db44cfc429e3e5cb9639a57d030444a
djsumin <136299367+djsumin@users.noreply.github.com>
2024-10-29 07:32:44 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.51 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -7,9 +7,11 @@ class Solution {
         
         for(int i=0;i<arr1.length;i++){
             for(int j=0;j<arr2[0].length;j++){
-                for(int k=0;k<arr1[0].length;k++){
-                    answer[i][j] += arr1[i][k] * arr2[k][j];    
+                int tmp = 0;
+                for(int k=0;k<arr2.length;k++){
+                    tmp += arr1[i][k] * arr2[k][j]; 
                 }
+                answer[i][j] = tmp;
             }
         }
         
